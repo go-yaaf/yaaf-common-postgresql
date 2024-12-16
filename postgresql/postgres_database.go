915b5ec5dc2791ad857b9d74f39ab16e5bc054f6
Motty Cohen <motty.cohen@gmail.com>
2024-12-16 15:57:31 +0200

    Handle table name placeholders

--- postgresql/postgres_database.go
+++ postgresql/postgres_database.go
@@ -240,32 +240,25 @@ func tableName(table string, keys ...string) (tblName string) {
 
 	tblName = table
 
-	//if len(keys) == 0 {
-	if strings.Contains(tblName, "-{") {
-		idx := strings.Index(tblName, "{")
-		return tblName[:idx-1]
-	} else {
-		return tblName
+	// If keys are not provided, remove placeholders
+	if len(keys) == 0 {
+		if strings.Contains(tblName, "{") {
+			idx := strings.Index(tblName, "{")
+			return tblName[:idx-1]
+		} else {
+			return tblName
+		}
 	}
-	//}
 
-	// replace accountId placeholder with the first key
-	//tblName = strings.Replace(tblName, "{accountId}", "{0}", -1)
+	// If keys provided, replace {key} placeholder with the first key
+	tblName = strings.ReplaceAll(table, "{key}", keys[0])
 
-	//for idx, key := range keys {
-	//	placeHolder := fmt.Sprintf("{%d}", idx)
-	//	tblName = strings.Replace(tblName, placeHolder, key, -1)
-	//}
-
-	// Replace templates: {{year}}
-	//tblName = strings.Replace(tblName, "{YYYY}", time.Now().Format("2006"), -1)
-
-	// Replace templates: {{month}}
-	//tblName = strings.Replace(tblName, "{MM}", time.Now().Format("01"), -1)
-
-	// TODO: Replace templates: {{week}}
-
-	//return
+	// If multiple keys provided as numbers (e.g. {0}, {1}, {2})
+	for idx, key := range keys {
+		placeHolder := fmt.Sprintf("{%d}", idx)
+		tblName = strings.Replace(tblName, placeHolder, key, -1)
+	}
+	return
 }
 
 //endregion
